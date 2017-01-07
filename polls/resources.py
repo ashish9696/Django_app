f52b97f4509ca63c16e20699c02b875464ddc4b0
ashish9696 <am67663@gmail.com>
2017-01-08 00:09:53 +0530

    Add files via upload

---
+++ polls/resources.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+# Create your tests here.
+from import_export import resources
+from .models import Person
+
+class PersonResource(resources.ModelResource):
+    class Meta:
+        model = Person
